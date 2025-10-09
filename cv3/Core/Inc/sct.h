2443775740b39efa695e1bfc0de925b22c10f626
Ondrej Zikan <zikan.ondrej@gmail.com>
2025-10-09 08:43:05 +0200

    display_BYE

---
+++ cv3/Core/Inc/sct.h
@@ -0,0 +1,14 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 9, 2025
+ *      Author: 247821
+ */
+
+#ifndef SRC_SCT_H_
+#define SRC_SCT_H_
+
+void sct_init();
+void sct_led(uint32_t value);
+
+#endif /* SRC_SCT_H_ */
