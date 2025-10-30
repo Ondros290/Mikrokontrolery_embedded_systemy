0d6ecaf7e7d31cca730727fe4c3ec614fbf627e9
Ondrej Zikan <zikan.ondrej@gmail.com>
2025-10-30 11:01:09 +0100

    cviceni6

---
+++ cv6/Core/Inc/sct.h
@@ -0,0 +1,16 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 9, 2025
+ *      Author: 247821
+ */
+
+#ifndef SRC_SCT_H_
+#define SRC_SCT_H_
+
+void sct_init();
+void sct_led(uint32_t value);
+void sct_value(uint16_t value, uint16_t decimal, uint8_t led);
+
+
+#endif /* SRC_SCT_H_ */
